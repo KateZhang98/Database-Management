ed4843a6e4902523404c54e65e612ba2824589e2
KateZhang98 <54541538+KateZhang98@users.noreply.github.com>
2022-06-02 16:19:02 -0500

    Add files via upload

---
+++ hw1-kate/src/hw1/Tuple.java
@@ -0,0 +1,96 @@
+package hw1;
+
+import java.sql.Types;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class represents a tuple that will contain a single row's worth of information
+ * from a table. It also includes information about where it is stored
+ * @author Sam Madden modified by Doug Shook
+ *
+ */
+public class Tuple {
+	 
+	/**
+	 * Creates a new tuple with the given description
+	 * @param t the schema for this tuple
+	 */
+	private TupleDesc desc;
+	private int pid;
+	private int id;
+	private Map<Integer, Field> field = new HashMap<Integer, Field>();
+	public Tuple(TupleDesc t) {
+		//your code here
+		this.desc = t;
+		this.pid=0;
+	}
+	
+	public TupleDesc getDesc() {
+		//your code here
+		return this.desc;
+	}
+	
+	/**
+	 * retrieves the page id where this tuple is stored
+	 * @return the page id of this tuple
+	 */
+	public int getPid() {
+		//your code here
+		return this.pid;
+	}
+
+	public void setPid(int pid) {
+		//your code here
+		this.pid=pid;
+	}
+
+	/**
+	 * retrieves the tuple (slot) id of this tuple
+	 * @return the slot where this tuple is stored
+	 */
+	public int getId() {
+		//your code here
+		return this.id;
+	}
+
+	public void setId(int id) {
+		//your code here
+		this.id=id;
+	}
+	
+	public void setDesc(TupleDesc td) {
+		//your code here;
+		this.desc =td;
+	}
+	
+	/**
+	 * Stores the given data at the i-th field
+	 * @param i the field number to store the data
+	 * @param v the data
+	 */
+	public void setField(int i, Field v) {
+		//your code here
+		this.field.put(i, v);
+	}
+	
+	public Field getField(int i) {
+		//your code here
+		return this.field.get(i);
+	}
+	
+	/**
+	 * Creates a string representation of this tuple that displays its contents.
+	 * You should convert the binary data into a readable format (i.e. display the ints in base-10 and convert
+	 * the String columns to readable text).
+	 */
+	public String toString() {
+		//your code here
+		StringBuilder s = new StringBuilder();
+		for(int i= 0;i<this.field.size();i++) {
+			s.append(getField(i));
+		}
+		return s.toString();
+	}
+}
+	
---
+++ hw1-kate/src/test/YourUnitTests.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import hw1.Catalog;
+import hw1.Database;
+import hw1.HeapFile;
+import hw1.HeapPage;
+import hw1.TupleDesc;
+
+public class YourUnitTests {
+	
+	private HeapFile hf;
+	private TupleDesc td;
+	private Catalog c;
+	private HeapPage hp;
+
+	@Before
+	public void setup() {
+		
+		try {
+			Files.copy(new File("testfiles/test.dat.bak").toPath(), new File("testfiles/test.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			System.out.println("unable to copy files");
+			e.printStackTrace();
+		}
+		
+		c = Database.getCatalog();
+		c.loadSchema("testfiles/test.txt");
+		
+		int tableId = c.getTableId("test");
+		td = c.getTupleDesc(tableId);
+		hf = c.getDbFile(tableId);
+		hp = hf.readPage(0);
+	}
+	
+	@Test
+	public void testNumSlots() throws Exception{
+		assertTrue("HeapPage getNumSlots() not working", hp.getNumSlots() == Math.floor(4096*8/(133*8+1)));
+	}
+	
+	@Test
+	public void testHeaderSize() throws Exception{
+		assertTrue("HeapPage getHeaderSize() not working", hp.getHeaderSize() == 4);
+		
+	}
+
+}
